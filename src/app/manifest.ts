30ab329f536d6df2d6e5b56fc6bbc488c44a2065
Jesus Ruiz Lopez <developjesus2601@gmail.com>
2026-04-13 01:17:02 -0600

    Fix case-sensitive PWA icon path

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -12,13 +12,13 @@ export default function manifest(): MetadataRoute.Manifest {
     orientation: 'portrait',
     icons: [
       {
-        src: '/ICONO.PNG',
+        src: '/icono.png',
         sizes: '192x192',
         type: 'image/png',
         purpose: 'maskable',
       },
       {
-        src: '/ICONO.PNG',
+        src: '/icono.png',
         sizes: '512x512',
         type: 'image/png',
       },
